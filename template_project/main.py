ddfa3db96ece3eed47e27ebd04c1960dc45cc635
MIM <admin@tvetgo.pro>
2025-07-16 03:45:08 +0800

    Update main.py

--- template_project/main.py
+++ template_project/main.py
@@ -1,5 +1,11 @@
+## Create main.py with contoh kod permulaan
 print("Hello Dunia dari Belia Nusantara! 🌿")
 print("Saya pelajar digital yang bersedia melangkah maju!")
-## Create main.py with contoh kod permulaan
+def salam_nusantara(nama):
+    print(f"Selamat datang, {nama}! Mari bina dunia digital bersama.")
+
+salam_nusantara("Herman")
+
+
 
 
